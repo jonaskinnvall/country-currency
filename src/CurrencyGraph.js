be8335a00a98183845d93ba44bfb828360024e7c
Jonas Kinnvall <jonaskinnvall@gmail.com>
2021-02-04 11:29:42 +0100

    Fix issue #2 Implementing a currency graph
    -Feature
    - A graph, from Recharts, now visualize the timeseries data spanning a
      month
    -New useState hook and still useEffect dep array issues

--- src/CurrencyGraph.js
+++ src/CurrencyGraph.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { AreaChart, Area, XAxis, YAxis, Tooltip } from 'recharts';
 import PropTypes from 'prop-types';
 
 const CurrencyGraph = ({ currency }) => {
     const [RateHistory, setRateHistory] = useState();
-    let rates = [];
+    const [Rates, setRates] = useState([]);
 
     useEffect(() => {
         const fetchRateHistory = async () => {
             try {
                 const response = await fetch(
-                    'https://api.exchangerate.host/timeseries?start_date=2020-01-01&end_date=2020-01-04&base=SEK&symbols=' +
+                    'https://api.exchangerate.host/timeseries?start_date=2020-01-01&end_date=2020-04-01&base=SEK&symbols=' +
                         currency
                 );
                 const rateData = await response.json();
@@ -18,26 +19,66 @@ const CurrencyGraph = ({ currency }) => {
                 console.log('Fetching error', err);
             }
         };
+        setRates([]);
         fetchRateHistory();
     }, [currency]);
 
     useEffect(() => {
-        console.log('RateHistory', RateHistory);
         if (RateHistory) {
-            for (const rate in RateHistory.rates) {
-                if (Object.hasOwnProperty.call(RateHistory.rates, rate)) {
+            for (const date in RateHistory.rates) {
+                if (Object.hasOwnProperty.call(RateHistory.rates, date)) {
                     const dateNRate = {
-                        date: rate,
-                        rate: RateHistory.rates[rate][currency],
+                        date: date,
+                        rate: RateHistory.rates[date][currency],
                     };
-                    rates.push(dateNRate);
+                    setRates((Rates) => [...Rates, dateNRate]);
                 }
             }
         }
-        console.log('rates', rates);
     }, [RateHistory]);
 
-    return <>{currency ? <div>{currency}</div> : <div></div>}</>;
+    return (
+        <>
+            {currency && Rates.length !== 0 ? (
+                <div>
+                    <AreaChart width={400} height={145} data={Rates}>
+                        <defs>
+                            <linearGradient
+                                id="colorRate"
+                                x1="0"
+                                y1="0"
+                                x2="0"
+                                y2="1"
+                            >
+                                <stop
+                                    offset="5%"
+                                    stopColor="#faa5aa"
+                                    stopOpacity={0.8}
+                                />
+                                <stop
+                                    offset="95%"
+                                    stopColor="#faa5aa"
+                                    stopOpacity={0}
+                                />
+                            </linearGradient>
+                        </defs>
+                        <XAxis dataKey="date" />
+                        <YAxis domain={['dataMin', 'dataMax']} />
+                        <Tooltip />
+                        <Area
+                            type="monotone"
+                            dataKey="rate"
+                            stroke="#091668"
+                            fillOpacity={1}
+                            fill="url(#colorRate)"
+                        />
+                    </AreaChart>
+                </div>
+            ) : (
+                <div></div>
+            )}
+        </>
+    );
 };
 
 CurrencyGraph.propTypes = {
