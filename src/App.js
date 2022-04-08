6d3679184c50d57c3c08b0c703f2fbb46e38fa38
Jonas Kinnvall <jonaskinnvall@gmail.com>
2022-04-08 21:59:05 +0200

    feat(lazy-load): Add lazy loading with Suspense
    - Lazy load CountryCard component in App with Suspense

--- src/App.js
+++ src/App.js
@@ -1,89 +1,97 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, lazy, Suspense } from 'react';
 import { DateTime } from 'luxon';
 
 import './styles.css';
-import CountryCard from './CountryCard';
 import InputField from './InputField';
+const CountryCard = lazy(() => import('./CountryCard'));
 
 async function fetchCountry(query) {
-    const cache = localStorage.getItem(query);
-    if (cache) {
-        return JSON.parse(cache);
-    }
+	const cache = localStorage.getItem(query);
+	if (cache) {
+		return JSON.parse(cache);
+	}
 
-    const response = await fetch(
-        'https://restcountries.com/v2/name/' + query + '?fullText=true'
-    );
-    if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem(query, JSON.stringify(data));
-        return data;
-    }
-    throw Error(`Request rejected with status ${response.status}`);
+	const response = await fetch(
+		'https://restcountries.com/v2/name/' + query + '?fullText=true'
+	);
+	if (response.ok) {
+		const data = await response.json();
+		localStorage.setItem(query, JSON.stringify(data));
+		return data;
+	}
+	throw Error(`Request rejected with status ${response.status}`);
 }
 
 export default function App() {
-    const [country, setCountry] = useState();
-    const [search, setSearch] = useState();
-    const [error, setError] = useState();
-    const today = DateTime.now();
-    const inputRef = useRef('');
+	const [country, setCountry] = useState();
+	const [search, setSearch] = useState();
+	const [error, setError] = useState();
+	const today = DateTime.now();
+	const inputRef = useRef('');
 
-    if (today.toISODate() !== JSON.parse(localStorage.getItem('date'))) {
-        localStorage.clear();
-        localStorage.setItem('date', JSON.stringify(today.toISODate()));
-    }
+	if (today.toISODate() !== JSON.parse(localStorage.getItem('date'))) {
+		localStorage.clear();
+		localStorage.setItem('date', JSON.stringify(today.toISODate()));
+	}
 
-    // UseEffect to fetch country when user types in search field
-    useEffect(() => {
-        if (search) {
-            setError(null);
-            fetchCountry(search).then(
-                (country) => {
-                    setCountry(country[0]);
-                    if (inputRef.current !== '') {
-                        inputRef.current.value = '';
-                    }
-                },
-                (error) => {
-                    setError(error.message);
-                }
-            );
-        }
-    }, [search]);
+	// UseEffect to fetch country when user types in search field
+	useEffect(() => {
+		if (search) {
+			setError(null);
+			fetchCountry(search).then(
+				(country) => {
+					setCountry(country[0]);
+					if (inputRef.current !== '') {
+						inputRef.current.value = '';
+					}
+				},
+				(error) => {
+					setError(error.message);
+				}
+			);
+		}
+	}, [search]);
 
-    return (
-        <>
-            <div className="App Navbar">
-                <h1>Country Currency</h1>
-                <InputField
-                    inputType={'Search'}
-                    inputValue={search}
-                    setInput={setSearch}
-                />
-            </div>
+	return (
+		<>
+			<div className="App Navbar">
+				<h1>Country Currency</h1>
+				<InputField
+					inputType={'Search'}
+					inputValue={search}
+					setInput={setSearch}
+				/>
+			</div>
 
-            {error ? (
-                <div className="App">
-                    <h2>
-                        Fetching error...{error} <br />
-                        Country name or country code not available. Please try
-                        another.
-                    </h2>
-                </div>
-            ) : !country ? (
-                <div className="App">
-                    <h2>Search for a country to learn more about it!</h2>
-                </div>
-            ) : (
-                <div className="App">
-                    <CountryCard
-                        countryInfo={country}
-                        today={today}
-                        ref={inputRef}
-                    />
-                </div>
-            )}
-        </>
-    );
+			{error ? (
+				<div className="App">
+					<h2>
+						Fetching error...{error} <br />
+						Country name or country code not available. Please try
+						another.
+					</h2>
+				</div>
+			) : !country ? (
+				<div className="App">
+					<h2>Search for a country to learn more about it!</h2>
+				</div>
+			) : (
+				<div className="App">
+					<Suspense
+						fallback={
+							<div className="App">
+								<h2>Fetching...</h2>
+							</div>
+						}
+					>
+						<CountryCard
+							countryInfo={country}
+							today={today}
+							ref={inputRef}
+						/>
+					</Suspense>
+				</div>
+			)}
+		</>
+	);
 }
